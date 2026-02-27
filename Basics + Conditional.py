0d5c603f5efb35ecb45c46b1c038ee6cc16f98cb
PrawutSa <prawutsaratontiphong@gmail.com>
2026-02-27 19:23:15 +1100

    Fixed screenshot name file

---
+++ Basics + Conditional.py
@@ -0,0 +1,12 @@
+# ตัวอย่าง
+name = "Prawut"       # string
+age = 23              # int
+height = 1.73         # float
+is_student = True     # boolean
+
+print(name, age, height, is_student)
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
